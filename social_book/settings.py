e546845785f8b73fd0326e927cf8c1dcc624ce57
Sonali Singh <sonali.singh.che22@itbhu.ac.in>
2024-12-27 22:13:17 +0530

    database added

--- social_book/settings.py
+++ social_book/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,7 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 # django_heroku.settings(locals())
 
