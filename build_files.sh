cd3979c69b02c8e98b0a2739668ee7b61d79d6e8
Sonali Singh <sonali.singh.che22@itbhu.ac.in>
2024-12-28 14:08:43 +0530

    Fix build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
 pip install -r requirements.txt
-python3.13 manage.py collectstatic
+# python3.13 manage.py collectstatic
+
+#!/bin/bash
+# echo "Collecting static files"
+# python3 manage.py collectstatic --noinput
+# echo "Build complete"
+
+#!/bin/bash
+echo "Collecting static files"
+python3 manage.py collectstatic --noinput
+echo "Build complete"
